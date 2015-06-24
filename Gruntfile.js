b4f117a60b9e3b0de4e206f9a0a3f070535b8b67
Rajs <flarecore@gmail.com>
2015-06-24 22:48:53 +0530

    Initial import

---
+++ Gruntfile.js
@@ -0,0 +1,116 @@
+'use strict';
+module.exports = function(grunt) {
+
+  require('time-grunt')(grunt);
+  require('load-grunt-tasks')(grunt);
+
+  // Project configuration.
+  grunt.initConfig({
+
+    config: {
+      src: 'src',
+      dist: 'dist'
+    },
+
+    watch: {
+      assemble: {
+        files: ['<%= config.src %>/{content,data,templates}/{,*/}*.{md,hbs,yml}'],
+        tasks: ['assemble']
+      },
+      livereload: {
+        options: {
+          livereload: '<%= connect.options.livereload %>'
+        },
+        files: [
+          '<%= config.dist %>/{,*/}*.html',
+          '<%= config.dist %>/assets/{,*/}*.css',
+          '<%= config.dist %>/assets/{,*/}*.js',
+          '<%= config.dist %>/assets/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
+        ]
+      }
+    },
+
+    connect: {
+      options: {
+        port: 9000,
+        livereload: 35729,
+        // change this to '0.0.0.0' to access the server from outside
+        hostname: 'localhost'
+      },
+      livereload: {
+        options: {
+          open: true,
+          base: [
+            '<%= config.dist %>'
+          ]
+        }
+      }
+    },
+
+    assemble: {
+      pages: {
+        options: {
+          flatten: true,
+          assets: '<%= config.dist %>/assets',
+          layoutdir: 'src/layouts/',
+          data: '<%= config.src %>/data/*.{json,yml}',
+          partials: 'src/partials/*.html'
+        },
+        files: {
+          '<%= config.dist %>/': ['src/pages/*.html']
+        }
+      }
+    },
+
+    copy: {
+      js: {
+        files: [{
+          expand: true,
+          cwd: 'bower_components/bootstrap/dist/',
+          src: ['js/*'],
+          dest: 'dist/assets/'
+        }, {
+          expand: true,
+          cwd: 'bower_components/bootstrap/dist/fonts/',
+          src: ['**'],
+          dest: 'dist/assets/fonts/bootstrap/'
+        }, {
+          expand: true,
+          cwd: 'src/assets/images/',
+          src: ['**'],
+          dest: 'dist/assets/images/'
+        }]
+      }
+    },
+    sass: {
+      dist: {
+        options: {
+          update: true,
+          sourcemap: 'inline',
+          style: 'compressed',
+          loadPath: "bower_components/bootstrap-sass-official/assets/stylesheets"
+        },
+        files: {
+          'dist/assets/css/style.css' : 'src/assets/scss/morrisons-petrol.scss'
+        }
+      }
+    },
+    clean: ['<%= config.dist %>/**/*.{html,xml}']
+
+  });
+
+  grunt.loadNpmTasks('assemble');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+
+  grunt.registerTask('build', [
+    'clean',
+    'copy',
+    'sass',
+    'assemble'
+  ]);
+
+  grunt.registerTask('default', [
+    'build'
+  ]);
+
+};
