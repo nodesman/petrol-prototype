b2b5b82a2345e389e35d0595429c055f4a6afc53
Rajs <flarecore@gmail.com>
2015-08-05 17:55:15 +0530

    Adjusted the retail prices rendering to stick headers on individual tables

--- Gruntfile.js
+++ Gruntfile.js
@@ -98,7 +98,7 @@ module.exports = function(grunt) {
       },
       dist: {
         files: {
-          'dist/assets/js/all.js': ['src/assets/js/jquery.min.js', 'bower_components/bootstrap/dist/js/bootstrap.min.js', 'src/assets/js/app.js']
+          'dist/assets/js/all.js': ['src/assets/js/jquery.min.js', 'src/assets/js/jquery.stickytableheaders.js', 'bower_components/bootstrap/dist/js/bootstrap.min.js', 'src/assets/js/app.js']
         }
       }
     },
